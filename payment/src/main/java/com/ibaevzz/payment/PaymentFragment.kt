9e09cdbe41aeebb69c79d10a1a22c9039404ce63
Ibaev Zubai <ibaevzubic2004@gmail.com>
2023-09-14 05:04:21 +0300

    Добавил ввод номера

--- payment/src/main/java/com/ibaevzz/payment/PaymentFragment.kt
+++ payment/src/main/java/com/ibaevzz/payment/PaymentFragment.kt
@@ -2,8 +2,11 @@ package com.ibaevzz.payment
 
 import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.OnFocusChangeListener
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
@@ -16,6 +19,11 @@ import javax.inject.Inject
 class PaymentFragment: Fragment() {
 
     private lateinit var binding: PaymentFragmentBinding
+    private val mask = "(***)***-**-**"
+    private val editText by lazy{
+        binding.phone.editText!!
+    }
+    private val phone = MutableList(10){-1}
 
     @Inject
     lateinit var viewModelFactory: ViewModelFactory
@@ -41,7 +49,7 @@ class PaymentFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding.pay.setOnClickListener{
-
+            //TODO
         }
         binding.root.setOnRefreshListener {
             viewModel.updateHotelModel()
@@ -51,5 +59,116 @@ class PaymentFragment: Fragment() {
             binding.root.isRefreshing = false
             binding.screen.visibility = View.VISIBLE
         }
+        editText.addTextChangedListener(PhoneTextWatcher())
+        editText.onFocusChangeListener = object: OnFocusChangeListener{
+            var isFirstFocus = true
+            override fun onFocusChange(view: View, isFocus: Boolean) {
+                if(isFirstFocus){
+                    editText.setText(mask)
+                    isFirstFocus = false
+                }
+            }
+        }
+    }
+
+    private inner class PhoneTextWatcher: TextWatcher {
+        var selection = 1
+        var len = 0
+        var num = ""
+
+        override fun onTextChanged(str: CharSequence, start: Int, before: Int, count: Int) {
+            if(len == str.length){
+                return
+            }
+
+            if(str.length<=14)
+            len = str.length
+
+            var i = 0
+            while(i<phone.size){
+                phone[i]=-1
+                i+=1
+            }
+
+            var xx = 0
+            i = 0
+            for(s in str){
+                if(s.toString()=="("||s.toString()==")"||s.toString()=="-")
+                    xx+=1
+                if(s.isDigit()&&i<10){
+                    phone[i] = s.digitToInt()
+                    i+=1
+                }
+            }
+
+            var isM = false
+
+            if(xx<4){
+                isM = true
+            }
+
+            selection = 0
+            var m = ""
+            var isS = false
+            var isD = false
+            var firstP = false
+            var secondP = false
+            var c = 0
+            var isDigit = true
+
+            for(i in 1..14){
+                if(c==0&&!isS){
+                    m+="("
+                    isS=true
+                    if(isDigit){
+                        selection+=1
+                    }
+                }else if(c==3&&!isD){
+                    m+=")"
+                    isD = true
+                    if(isDigit){
+                        selection+=1
+                    }
+                }else if(c==6&&!firstP){
+                    m+="-"
+                    firstP = true
+                    if(isDigit){
+                        selection+=1
+                    }
+                }else if(c==8&&!secondP){
+                    m+="-"
+                    secondP = true
+                    if(isDigit){
+                        selection+=1
+                    }
+                }
+                else if(c<10){
+                    if(isM&&c<9) {
+                        m += if (phone[c + 1] > -1) {
+                            selection += 1
+                            phone[c].toString()
+                        } else {
+                            isDigit = false
+                            "*"
+                        }
+                    }else{
+                        m += if (phone[c] > -1) {
+                            selection += 1
+                            phone[c].toString()
+                        } else {
+                            isDigit = false
+                            "*"
+                        }
+                    }
+                    c+=1
+                }
+            }
+            num = m
+            editText.setText(m)
+            editText.setSelection(selection)
+        }
+
+        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+        override fun afterTextChanged(s: Editable) {}
     }
 }
